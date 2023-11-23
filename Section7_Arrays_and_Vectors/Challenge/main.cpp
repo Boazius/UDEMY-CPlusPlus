b5bf64c27580518ffb4627b960e5dfc9d978928e
Boaz Yakubov <Boazyakubov@gmail.com>
2023-11-23 18:48:55 +0200

    finished challenge 7

---
+++ Section7_Arrays_and_Vectors/Challenge/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    using std::vector;
+    using std::cout;
+    using std::endl;
+    using std::cin;
+
+    vector<int> vector1;
+    vector<int> vector2;
+    vector1.push_back(10);
+    vector1.push_back(20);
+    cout << "vector1 is {" << vector1.at(0) << "," << vector1.at(1) << "}\n";
+    cout << "The size of vector1 is " << vector1.size() << endl;
+
+    vector2.push_back(100);
+    vector2.push_back(200);
+    cout << "vector2 is {" << vector2.at(0) << "," << vector2.at(1) << "}\n";
+    cout << "The size of vector2 is " << vector2.size() << endl;
+
+    vector<vector<int>> vector_2d;
+    vector_2d.push_back(vector1);
+    vector_2d.push_back(vector2);
+
+    // display vector_2d:
+    cout << "vector_2d is \n{\n" <<
+                             "   {" << vector_2d.at(0).at(0) << ',' << vector_2d.at(0).at(1) << "},\n"<<
+                             "   {" << vector_2d.at(1).at(0) << ',' << vector_2d.at(1).at(1) << "}\n" <<
+                             "}\n";
+    
+
+    vector1.at(0) = 1000;
+
+
+    // display vector_2d:
+    cout << "vector_2d is \n{\n" <<
+                             "   {" << vector_2d.at(0).at(0) << ',' << vector_2d.at(0).at(1) << "},\n"<<
+                             "   {" << vector_2d.at(1).at(0) << ',' << vector_2d.at(1).at(1) << "}\n" <<
+                             "}\n";
+    
+    cout << "vector1 is {" << vector1.at(0) << "," << vector1.at(1) << "}\n";
+    
+
+
+
+    return 0;
+}
